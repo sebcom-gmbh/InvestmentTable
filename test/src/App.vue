1263b6295b78c2421c7cd00255dc32dbe948efca
Sebastian Drozdz <sebastian.drozdz@sebcom.ch>
2020-10-05 17:35:18 +0200

    init commit

---
+++ test/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div class="ui container">
+      <investment-table></investment-table>
+    </div>
+  </div>
+</template>
+
+<script>
+import InvestmentTable from './components/InvestmentTable'
+
+export default {
+  name: 'app',
+  components: {
+    InvestmentTable
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
